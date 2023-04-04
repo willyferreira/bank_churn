0bc8853b1e12c218abfc035e6610f24ffbe5078e
Willy Ferreira <40251830+willyferreira@users.noreply.github.com>
2023-04-04 12:25:47 -0300

    opa

--- bank_churn.py
+++ bank_churn.py
@@ -1,9 +1,40 @@
 #1. Instalação e importação de pacotes
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
 #2.Leitura do dataframe
-pd.read_csv(filepath_or_buffer = 'churn.csv', encoding = 'UTF-8')
+df = pd.read_csv(filepath_or_buffer = 'churn.csv', encoding = 'UTF-8')
+
+    #2.1. Manipulação do dataframe
+    #Exclusão da coluna RowNumber
+    df.drop(columns = {'RowNumber'}, inplace = True) 
+    df.head()
+
+    #Alteração do nome das colunas 
+    df.rename(columns = {
+        'CustomerId': 'Id_Cliente',
+        'Surname': 'Nome',
+        'CreditScore': 'Score_Credito',
+        'Geography': 'Nacionalidade',
+        'Gender': 'Genero',
+        'Age': 'Idade',
+        'Tenure': 'Tempo_Fidelidade',
+        'Balance': 'Saldo',
+        'NumOfProducts': 'Qtd_Produtos',
+        'HasCrCard': 'Tem_Cartao',
+        'IsActiveMember': 'Cliente_Ativo',
+        'EstimatedSalary': 'Salario_Estimado',
+        'Exited': 'Ex_Cliente'},
+        inplace = True
+        )
+    df.head()
+
+   plt.boxplot(
+    x = df['Score_Credito']
+    notch = True,
+    vert = False
+   )
+
